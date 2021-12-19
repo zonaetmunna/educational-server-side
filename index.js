a0187456164d26761a89cb458b49653557adc2ae
zonaetmunna <zonaetmonna@gmail.com>
2021-12-19 19:28:32 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require('cors');
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+     res.send('hi everyone');
+});
+app.listen(port, () => {
+     console.log('listing the port', port);
+})
