1c50d294c516bcb81e2982d962dcf541b7d30261
zonaetmunna <zonaetmonna@gmail.com>
2022-02-20 00:36:07 +0600

    add

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 const express = require("express");
 const cors = require('cors');
 const app = express();
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+
 
 const port = process.env.PORT || 5000;
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+// database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fuuny.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+     try {
+          await client.connect();
+          const database = client.db('educational');
+          const coursesCollection = database.collection('courses')
+
+     }
+     finally {
+          // await client.close();
+     }
+
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
      res.send('hi everyone');
